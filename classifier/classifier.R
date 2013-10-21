8f0430a3d2d9c572ddc942ea3add1bb4c7f1c15e
Mateusz Opala <mateuszjacekopala@gmail.com>
2013-10-21 11:53:30 +0200

    Add labels name, knn procedure

--- classifier/classifier.R
+++ classifier/classifier.R
@@ -7,14 +7,14 @@ path.to.data <- '../Data/rotated/Set1'
 LoadData <- function(path, size=65536) {
 #   data <- matrix(nrow=0, ncol=size)
   data <- list()
-  labels <- list()
+  labels <- c()
   file.names <- dir(path=path)
   for(i in seq(1:length(file.names))) {
     path.to.img <- paste(path, file.names[i],sep="/")
 #     features.vector <- as.vector(readPNG(path.to.img))
 #     data <- rbind(data, features.vector)
     data[[i]] <- readPNG(path.to.img)
-    labels[[i]] <- sub("[0-9].png", x, file.names[i])
+    labels[i] <- sub("[0-9].png", x, file.names[i])
   }
   list(data, labels)
 }
@@ -51,8 +51,56 @@ ConvertDataToFeatures <- function(data, wave.name, level) {
   featured.data
 }
 
+PrepareDataFrame <- function(featured.data, labels) {
+  data <- data.frame()
+  for(i in 1:length(featured.data)) {
+    data <- rbind(data, featured.data[[i]])
+  }
+  data <- cbind(data, labels)
+# Shuffle data   
+  data <- data[sample.int(nrow(data)),] 
+  n <- dim(data)[2]
+  colnames(data)[n] <- "Labels"
+  data
+}
+
+DoKNN <- function(data, dist=2, kSupremum=100, folds.count=5) {
+  m <- dim(data)[1]
+  n <- dim(data)[2]-1
+  
+  step <- as.integer(m/folds.count)
+  
+  errorRates <- matrix(0, nrow=kSupremum, ncol=folds.count)
+  
+  for(j in seq(1,m,by=step)) {
+    
+    fold.index <- as.integer(j/step)+1
+    
+    if( j+step >= m ) {
+      test.data <- data[j:m,]
+      train.data <- data[1:(j-1),]
+    }
+    else if( j == 1 ) {
+      test.data <- data[1:(j+step-1),]
+      train.data <- data[(j+step):m,]
+    }
+    else {
+      test.data <- data[j:(j+step-1),]
+      train.data <- rbind(data[1:(j-1),],data[(j+step):m,])
+    }
+    
+    for(i in 1:kSupremum) {
+      kknn.result <- kknn(Labels~., train.data, test.data, distance=dist, k=i)
+      fit <- fitted(kknn.result)
+      errorRate <- sum(as.integer(fit != test.data$Labels))/nrow(test.data)
+      errorRates[i,fold.index] <- errorRate
+    } 
+  }
+  apply(errorRates, 1, mean)
+}
+
 both.data.labels <- LoadData(path.to.data)
 data <- both.data.labels[[1]]
 labels <- both.data.labels[[2]]
 featured.data <- ConvertDataToFeatures(data, 'haar', 2)
-
+data <- PrepareDataFrame(featured.data, labels)
